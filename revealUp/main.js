e90d4802e8b998b5abba532a6e9b7e5d63f56f10
Theo R. <167128657+theoringmayr@users.noreply.github.com>
2024-09-28 16:54:09 +0200

    Add files via upload

---
+++ revealUp/main.js
@@ -0,0 +1,18 @@
+//Listen for clicks on original text
+const originalText = document.querySelectorAll(".original");
+const simpleText = document.querySelectorAll(".translation");
+
+originalText.forEach(element => {
+    element.addEventListener("click", () => {
+        // element.nextSibling.classList.add("show");
+        console.log(element.nextElementSibling);
+        element.nextElementSibling.classList.toggle("show");
+    });
+});
+
+simpleText.forEach(element => {
+    element.addEventListener("click", () => {
+        element.classList.toggle("show");
+    });
+});
+
